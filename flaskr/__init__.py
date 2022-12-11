cfdefc2b768651e3a959e69c276be1fc27a29da3
RODRIGO GALLEGO <50471819+ragallegom@users.noreply.github.com>
2022-12-11 16:15:47 -0500

    feat: create blog

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from . import db
 from . import auth
+from . import blog
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -24,6 +25,9 @@ def create_app(test_config=None):
     db.init_app(app)
 
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+
+    app.add_url_rule('/', endpoint='index')
 
     @app.route('/hello')
     def hello():
